c37581c25d08eb5ccacf23907610b8e6cba8f2e1
Adrien Lapasset <aflapasset@gmail.com>
2020-02-03 17:09:53 +0100

    update node-sass

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// mongoose.connect('mongodb://127.0.0.1:27017/habit-tracker', { useNewUrlParser: true });
-mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/habit-tracker', { useNewUrlParser: true });
+// mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
 	console.log('MongoDB database connection established successfully');
